0a9f49c685f94e1162fd78d8dd29046f9904bb8f
George1010 <48166214+George1010@users.noreply.github.com>
2020-07-06 19:07:06 +0530

    Update model1.py

--- model1.py
+++ model1.py
@@ -22,26 +22,24 @@ model=Sequential()
 
 model.add(Conv2D(200,(3,3),input_shape=data.shape[1:]))
 model.add(Activation('relu'))
-model.add(MaxPooling2D(pool_size=(2,2)))
-#The first CNN layer followed by Relu and MaxPooling layers
+model.add(MaxPooling2D(pool_size=(2,2))) 
 
 model.add(Conv2D(100,(3,3)))
 model.add(Activation('relu'))
 model.add(MaxPooling2D(pool_size=(2,2)))
-#The second convolution layer followed by Relu and MaxPooling layers
+
 model.add(Conv2D(100,(3,3)))
 model.add(Activation('relu'))
 model.add(MaxPooling2D(pool_size=(2,2)))
 
 model.add(Flatten())
 model.add(Dropout(0.5))
-#Flatten layer to stack the output convolutions from second convolution layer
+
 model.add(Dense(50,activation='relu'))
 model.add(Dense(50,activation='relu'))
 
-#Dense layer of 64 neurons
+
 model.add(Dense(2,activation='softmax'))
-#The Final layer with two outputs for two categories
 
 
 
